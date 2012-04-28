69c0e3e4a38385caa6289963bebebe66486425c9
wearetitans <wearetitans@coeus.local>
2012-04-28 07:49:55 -0400

    Initial Commit

---
+++ gpsswimjam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gpsswimjam
+//
+//  Created by wearetitans on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gpsswimjam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gpsswimjam
+//
+//  Created by wearetitans on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
